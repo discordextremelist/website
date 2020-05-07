7f431e0b1ddf94a937c149e1a285e8bc1c37d898
Cairo2k18 <cairoacason.john@gmail.com>
2020-05-08 00:39:45 +1200

    Bump issue and fix code quality issue

--- src/Util/Services/serverCaching.js
+++ src/Util/Services/serverCaching.js
@@ -17,10 +17,8 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-const ioRedis = require("ioredis");
 
 const app = require("../../../app.js");
-const settings = require("../../../settings.json");
 const prefix = "servers";
 
 async function getServer(id) {

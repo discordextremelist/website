aa07748058d1b1df9196c6b1bda176eabbbf8d5d
Cairo2k18 <cairoacason.john@gmail.com>
2020-06-14 07:14:30 -0400

    fixed some things and yeah

--- scripts/css_build.sh
+++ scripts/css_build.sh
@@ -22,20 +22,30 @@ echo "Would you like to build [1] Standard CSS, [2] Form CSS, [3] Search CSS, [4
 read input
 if [ $input = 1 ]
 then 
-    node-sass --omit-source-map-url src/Assets/Sass/main.scss src/Assets/Public/css/standard.css --output-style compressed 
+    node-sass --omit-source-map-url assets/Sass/main.scss assets/Public/css/standard.css --output-style compressed 
+    node-sass --omit-source-map-url assets/Sass/mainRTL.scss assets/Public/css/standard-rtl.css --output-style compressed 
 elif [ $input = 2 ]
 then
-    node-sass --omit-source-map-url src/Assets/Sass/form.scss src/Assets/Public/css/form.css --output-style compressed
+    node-sass --omit-source-map-url assets/Sass/form.scss assets/Public/css/form.css --output-style compressed
+    node-sass --omit-source-map-url assets/Sass/formRTL.scss assets/Public/css/form-rtl.css --output-style compressed
 elif [ $input = 3 ]
 then
-    node-sass --omit-source-map-url src/Assets/Sass/search.scss src/Assets/Public/css/search.css --output-style compressed
+    node-sass --omit-source-map-url assets/Sass/search.scss assets/Public/css/search.css --output-style compressed
+    node-sass --omit-source-map-url assets/Sass/searchRTL.scss assets/Public/css/search-rtl.css --output-style compressed
 elif [ $input = 4 ]
 then
-    node-sass --omit-source-map-url src/Assets/Sass/listing.scss src/Assets/Public/css/listing.css --output-style compressed
+    node-sass --omit-source-map-url assets/Sass/listing.scss assets/Public/css/listing.css --output-style compressed
+    node-sass --omit-source-map-url assets/Sass/listingRTL.scss assets/Public/css/listing-rtl.css --output-style compressed
 else
-    node-sass --omit-source-map-url src/Assets/Sass/main.scss src/Assets/Public/css/standard.css --output-style compressed 
-    node-sass --omit-source-map-url src/Assets/Sass/form.scss src/Assets/Public/css/form.css --output-style compressed
-    node-sass --omit-source-map-url src/Assets/Sass/search.scss src/Assets/Public/css/search.css --output-style compressed
-    node-sass --omit-source-map-url src/Assets/Sass/listing.scss src/Assets/Public/css/listing.css --output-style compressed
-    node-sass --omit-source-map-url src/Assets/Sass/all.scss src/Assets/Public/css/all.css --output-style compressed
+    node-sass --omit-source-map-url assets/Sass/main.scss assets/Public/css/standard.css --output-style compressed 
+    node-sass --omit-source-map-url assets/Sass/form.scss assets/Public/css/form.css --output-style compressed
+    node-sass --omit-source-map-url assets/Sass/search.scss assets/Public/css/search.css --output-style compressed
+    node-sass --omit-source-map-url assets/Sass/listing.scss assets/Public/css/listing.css --output-style compressed
+    node-sass --omit-source-map-url assets/Sass/all.scss assets/Public/css/all.css --output-style compressed
+
+    node-sass --omit-source-map-url assets/Sass/mainRTL.scss assets/Public/css/standard-rtl.css --output-style compressed 
+    node-sass --omit-source-map-url assets/Sass/formRTL.scss assets/Public/css/form-rtl.css --output-style compressed
+    node-sass --omit-source-map-url assets/Sass/searchRTL.scss assets/Public/css/search-rtl.css --output-style compressed
+    node-sass --omit-source-map-url assets/Sass/listingRTL.scss assets/Public/css/listing-rtl.css --output-style compressed
+    node-sass --omit-source-map-url assets/Sass/allRTL.scss assets/Public/css/all-rtl.css --output-style compressed
 fi

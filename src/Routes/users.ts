913efd5cecb39f992987176ba44eebb7a057d99b
Advaith <advaithj1@gmail.com>
2020-08-29 17:03:56 -0700

    Revert "Add types to all templates for loops"
    This reverts commit 61e9da0187cf52062b07f29ddb002bbc940c6d39.

--- src/Routes/users.ts
+++ src/Routes/users.ts
@@ -104,7 +104,7 @@ router.get("/:id", variables, async (req: Request, res: Response, next) => {
 
     const templatesOwner: delTemplate[] = [];
 
-    for (const template of templates as delTemplate[]) {
+    for (const template of templates) {
         if (req.params.id === template.owner.id) {
             templatesOwner.push(template);
         }

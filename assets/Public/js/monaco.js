e3ec3093bdb078c271b920c87e0ee063978350dc
Advaith <advaithj1@gmail.com>
2020-09-11 19:03:03 -0700

    load monaco from cdnjs

---
+++ assets/Public/js/monaco.js
@@ -0,0 +1,4 @@
+self.MonacoEnvironment = {
+    baseUrl: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.20.0/min/'
+};
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.20.0/min/vs/base/worker/workerMain.min.js');

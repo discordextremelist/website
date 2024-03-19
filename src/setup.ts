4a52164795d3c6a78dc51d12d1d5f7151ea60864
Carolina Mitchell <carolinamitchell@outlook.co.nz>
2024-03-19 18:29:54 +1300

    Simplify import of advaith's libs list

--- src/setup.ts
+++ src/setup.ts
@@ -17,7 +17,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import { libs } from "../node_modules/lib-comparison/libs";
+import { libs } from "lib-comparison/libs";
 
 async function setup() {
     console.log("Setup: Updating libraries...");

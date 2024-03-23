d15cfffa41af95433174bfb55c7838fb62cb68c4
Carolina Mitchell <carolinamitchell@outlook.co.nz>
2024-03-23 19:53:08 +1300

    Using GA through CF now, removing, slight changes for performance of script tags

--- src/Util/Function/variables.ts
+++ src/Util/Function/variables.ts
@@ -50,7 +50,6 @@ export const variables = async (
 
     res.locals.requestedAt = Date.now();
     res.locals.cssVersion = pkg.version;
-    res.locals.gaID = settings.website.gaID;
     res.locals.arcID = settings.website.arcID;
 
     res.locals.linkPrefix = `/${

2bd60efeb7a8def3993be39b264e95046138d0bd
Carolina Mitchell <carolinamitchell@outlook.co.nz>
2025-02-16 18:20:31 +1300

    Fix sentry integration to follow documentation

---
+++ src/instrument.ts
@@ -0,0 +1,27 @@
+import * as Sentry from "@sentry/node"
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+import settings from "../settings.json" with { type: "json" };
+
+if (!settings.website.dev) {
+    Sentry.init({
+        dsn: settings.secrets.sentry,
+        integrations: [
+          nodeProfilingIntegration(),
+        ],
+        release: "website@" + process.env.npm_package_version,
+        environment: "production",
+        // Tracing
+        tracesSampleRate: 0.1, //  Capture 10% of the transactions
+        profilesSampleRate: 0.1 // Set sampling rate for profiling - this is relative to tracesSampleRate
+    });
+}
+
+
+if (!settings.website.dev) {
+    Sentry.init({
+        dsn: settings.secrets.sentry,
+        integrations: [Sentry.expressIntegration()],
+        tracesSampleRate: 0.1,
+        profilesSampleRate: 0.1
+    });
+}
